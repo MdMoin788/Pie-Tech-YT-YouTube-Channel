848260e087250451a099dafd783210a8763a0bed
MdMoin788 <developermdmoin@gmail.com>
2022-08-03 02:52:06 +0530

    useState in react

--- Day-02 - useState in REACT/React/src/App.js
+++ Day-02 - useState in REACT/React/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-      Welcome to React ( Lecture- 04 ) React  Getting Started
+    Welcome to React ( Lecture- 05 ) React UseState() in React
+
       <br></br>
       <br></br>
+      <Home></Home>
     </div>
   );
 }
--- Day-02 - useState in REACT/React/src/Components/Home.jsx
+++ Day-02 - useState in REACT/React/src/Components/Home.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+  // write js code here.....
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [arr, setArr] = useState([fsfsdfdsfds])
+
+
+
+  const ShowData = () => {
+    let user = {
+      email: email,
+      password: password
+    }
+    setArr([...arr, user])
+
+
+  }
+
   return (
     <div>
-      
+      {/* Write html code here.... */}
+
+      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <input type="text" onChange={(e) => setPassword(e.target.value)} />
+
+      <button onClick={ShowData}>Show Data</button>
+
+      <div>
+        {
+          arr.map((ele) => {
+            return (
+              <>
+                <div>email : {ele.email} </div>
+                <div>password : {ele.password} </div>
+              </>
+            )
+          })
+
+        }
+      </div>
     </div>
   )
 }
